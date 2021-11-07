51f8f42a055c57b899cec6524fc8c18c098eff99
Vasil Stanchev <stanchev.service@gmail.com>
2021-11-07 20:59:25 +0200

    test

--- 22. Greater Number.py
+++
@@ -1,7 +0,0 @@
-furst_numer = int(input())
-second_numer = int(input())
-
-if furst_numer>second_numer:
-    print(furst_numer)
-else:
-    print(second_numer)
--- 24. Password Guess 1 ​.py
+++
@@ -1,7 +0,0 @@
-password=(input())
-
-true_password="s3cr3t!P@ssw0rd"
-if true_password==password:
-    print("Welcome")
-else:
-    print("Wrong password!")

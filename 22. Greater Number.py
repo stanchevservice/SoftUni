0aaa7a46ea39ac791f2217260a9a5689e3e3ad29
Vasil Stanchev <stanchev.service@gmail.com>
2021-11-07 21:00:45 +0200

    Conditional Statements - Lab

---
+++ 22. Greater Number.py
@@ -0,0 +1,7 @@
+furst_numer = int(input())
+second_numer = int(input())
+
+if furst_numer>second_numer:
+    print(furst_numer)
+else:
+    print(second_numer)
---
+++ 24. Password Guess 1 ​.py
@@ -0,0 +1,7 @@
+password=(input())
+
+true_password="s3cr3t!P@ssw0rd"
+if true_password==password:
+    print("Welcome")
+else:
+    print("Wrong password!")
